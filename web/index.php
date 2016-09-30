a7943ab02ca04d2b5c28ff6c48843ada25478359
Mark Lawson <mlawson3691@gmail.com>
2016-09-30 10:39:59 -0700

    add jQuery and Bootstrap fonts and javascript files

--- web/index.php
+++ web/index.php
@@ -9,6 +9,8 @@
     <meta charset="utf-8">
     <link href='/assets/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
     <link href='/assets/css/styles.css' rel='stylesheet' type='text/css'>
+    <script src='/assets/js/jquery-3.1.1.min.js'></script>
+    <script src='/assets/js/bootstrap.min.js'></script>
     <title>Shoe Locator</title>
 </head>
 </html>
