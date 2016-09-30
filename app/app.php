4fe111f02470bf5491500293e4664c46a0aaa0bd
Mark Lawson <mlawson3691@gmail.com>
2016-09-30 09:49:26 -0700

    create app.php with route to homepage and twig template

---
+++ app/app.php
@@ -0,0 +1,29 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+?>
