e4db6db944154dcac2f253d2d886017f36801698
Mark Lawson <mlawson3691@gmail.com>
2016-09-30 09:48:34 -0700

    create with index.php referencing app.php and links to stylesheets

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php
+    $website = require_once __DIR__.'/../app/app.php';
+    $website->run();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link href='/assets/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+    <link href='/assets/css/styles.css' rel='stylesheet' type='text/css'>
+    <title>Shoe Locator</title>
+</head>
+</html>
